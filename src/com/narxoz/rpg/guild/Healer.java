656363f7dadb1d7e187beee6fe397d13177d9d81
Nesibeli Gairatkyzy <nessibeli.gairatkyzy@narxoz.kz>
2026-05-09 16:34:56 +0500

    implement concrete GuildMembers

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -1,8 +1,6 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for wounds, potions, and recovery plans.
- */
+
 public class Healer extends GuildMember {
 
     public Healer(String name, GuildMediator mediator) {
@@ -10,12 +8,12 @@ public class Healer extends GuildMember {
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Healer " + getName() + "] Received " + topic +
+                " from " + from.getName() + ": " + payload);
     }
 }
